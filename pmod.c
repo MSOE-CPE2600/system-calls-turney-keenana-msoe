4d178b487ada842fb5e64bb05a6de8e830f58f64
Keenana <keenana@msoe.edu>
2024-11-14 22:35:21 -0600

    pmod done

--- pmod.c
+++ pmod.c
@@ -5,11 +5,26 @@
 * Date: 11/6/24
 * Note: Section 111
 *****************************************************************************/
-
+#include <unistd.h>
+#include <time.h>
+#include <stdio.h>
 // pmod.c
 
 int main(int argc, char* argv[])
 {
-
+    int current_nice = nice(0);
+    printf("starting priority: %d\n", current_nice);
+        current_nice = nice(-10);
+        printf("The new nice value is: %d\n",current_nice);
+    struct timespec req, rem;
+    req.tv_sec = 1;
+    req.tv_nsec = 837272638;
+    if (nanosleep(&req, &rem) == -1) {
+        perror("nanosleep failed");
+        printf("Remaining time: %ld seconds and %ld nanoseconds\n", rem.tv_sec, rem.tv_nsec);
+    } else {
+        printf("nanosleep completed successfully\n");
+        printf("goodbye.\n");
+    }
     return 0;
 }
